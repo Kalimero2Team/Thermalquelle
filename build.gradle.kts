caee9fa848130a204c4e47b5f12de778e20d061a
byquanton <32410361+byquanton@users.noreply.github.com>
2024-08-15 13:07:23 +0200

    Update Upstream & Remove Client Side Movement Patch

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("ca.stellardrift.gitpatcher") version "1.1.0"
 }
 
-patches {
+gitPatcher.patchedRepos.create("geyser") {
     submodule.set("Geyser")
     target.set(file("patched-geyser"))
     patches.set(file("patches"))
